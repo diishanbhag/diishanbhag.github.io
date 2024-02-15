8fcb842d57e8c92ad7f378c1f933ec225f06b001
Epsilon <101011193+KumarSampurn@users.noreply.github.com>
2024-02-16 03:18:09 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
 module.exports = {
-  webpack: (config, options) =>
-  {
-      config.module.rules.push({
-          test: /\.pdf$/i,
-          type: 'asset/source'
-      })
+  // Use the export option instead of the exportPathMap function
+  // exportPathMap: function() {
+  //   return {
+  //     '/': { page: '/' }
+  //   };
+  // }
+  output: 'export', // Use the 'export' option for static HTML export
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.pdf$/i,
+      type: 'asset/source'
+    });
 
-      return config
-  },
-}
+    return config;
+  }
+};
